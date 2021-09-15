e5b9b827fbbc1b9908a2bc283f06d9501385d4b3
Fsx <45301895+Ekko9@users.noreply.github.com>
2021-09-15 15:36:03 +0800

    Update get_city.py

--- get_city.py
+++ get_city.py
@@ -6,8 +6,6 @@ def get_CityName():
     try:
         res = requests.post(url='http://ip-api.com/json/?lang=zh-CN', data={'ip': 'myip'}, timeout=10)
         result = res.json()['city']
-        if result == '北京':
-           result = '上海'
     except:
         print(" [!]正在进行网络自检并重试")
         try:
